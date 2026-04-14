8a11c2abbcbf78dfd287ee93e4a90fef45da51f5
kwakinsung <kis2690@gmail.com>
2026-04-14 15:35:54 +0900

    feat: 커스텀 예외 setting

--- src/main/java/com/springboot/monew/exception/MonewException.java
+++ src/main/java/com/springboot/monew/exception/MonewException.java
@@ -1,4 +1,34 @@
 package com.springboot.monew.exception;
 
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
 public class MonewException extends RuntimeException {
+
+    private final Instant timestamp;
+    private final ErrorCode errorCode;
+    private final Map<String, Object> details;
+
+    public MonewException(ErrorCode errorCode){
+        super(errorCode.getMessage());
+        this.timestamp = Instant.now();
+        this.errorCode = errorCode;
+        this.details = new HashMap<>();
+    }
+
+    public MonewException(ErrorCode errorCode, Throwable cause) {
+        super(errorCode.getMessage(), cause);
+        this.timestamp = Instant.now();
+        this.errorCode = errorCode;
+        this.details = new HashMap<>();
+    }
+
+    public void addDetail(String key, Object value) {
+        this.details.put(key, value);
+    }
+
 }
