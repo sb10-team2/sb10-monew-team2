68ccd47410801625de8e713feb438282522b8ed2
kwakinsung <kis2690@gmail.com>
2026-04-14 09:49:46 +0900

    MoNew setting

---
+++ src/main/java/com/springboot/monew/MoNewApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.monew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoNewApplication.class, args);
+    }
+
+}
