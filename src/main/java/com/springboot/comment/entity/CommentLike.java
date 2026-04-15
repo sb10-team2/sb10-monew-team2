2bbcebc78fa750b64ae15470a3f1866c7218fa59
jsj <sungju3115@naver.com>
2026-04-16 07:25:09 +0900

    feat: CommentLike Entity 구현 및 Controller, Service, Repository 추가

---
+++ src/main/java/com/springboot/comment/entity/CommentLike.java
@@ -0,0 +1,29 @@
+package com.springboot.comment.entity;
+
+import com.springboot.monew.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name = "comment_likes")
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentLike extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id", nullable = false)
+    private Comment comment;
+
+    // TODO: User 구현 완료 시 주석 해제
+    // @ManyToOne(fetch = FetchType.LAZY)
+    // @JoinColumn(name = "user_id", nullable = false)
+    // private User user;
+
+    public CommentLike(Comment comment
+                       // , User user
+                       ) {
+        this.comment = comment;
+        // this.user = user;
+    }
+}
