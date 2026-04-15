ee714eef89a7900410248c5f5313029654279b86
kwakinsung <kis2690@gmail.com>
2026-04-15 19:02:23 +0900

    feat: 뉴스기사 Entity 설계

---
+++ src/main/java/com/springboot/monew/newsarticles/entity/NewsArticles.java
@@ -0,0 +1,64 @@
+package com.springboot.monew.newsarticles.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor //기본 생성자
+@Table(name = "news_articles",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "UK_NEWS_ARTICLES_ORIGINAL_LINK", columnNames = "original_link")
+        })
+public class NewsArticles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 100)
+    private String source;
+
+    @Column(name = "original_link", nullable = false, length = 500)
+    private String originalLink;
+
+    @Column(nullable = false, length = 300)
+    private String title;
+
+    @Column(name = "published_at", nullable = false)
+    private Instant publishedAt;
+
+    @Column(nullable = false, columnDefinition = "TEXT")    //JPA가 테이블 생성할때 해당 컬럼을 DB의 TEXT 타입으로 지정하라.
+    private String summary;
+
+    @Column(name = "view_count", nullable = false)
+    private int viewCount = 0;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
+
+    public NewsArticles(String source, String originalLink, String title, Instant publishedAt, String summary) {
+        this.source = source;
+        this.originalLink = originalLink;
+        this.title = title;
+        this.publishedAt = publishedAt;
+        this.summary = summary;
+        this.viewCount = 0;
+        this.isDeleted = false;
+    }
+
+//    // ===== 비즈니스 로직 =====
+//    public void increaseViewCount() {
+//        this.viewCount++;
+//    }
+//
+//    public void delete() {
+//        this.isDeleted = true;
+//    }
+
+
+}
