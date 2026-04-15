2dc2075dfe9836a5e1abf8602188c102e76a03f1
jsj <sungju3115@naver.com>
2026-04-16 07:29:31 +0900

    refactor: comment 패키지 위치 수정

--- src/main/java/com/springboot/comment/service/CommentService.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.comment.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class CommentService {
-}
