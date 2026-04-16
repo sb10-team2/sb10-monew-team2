f5b22336dfa17c1d6d51c23e45d2d12a88d8e023
jsj <sungju3115@naver.com>
2026-04-16 13:45:26 +0900

    feat: 댓글 좋아요 기능 구현(연관관계 매핑 추가 필요)

--- src/main/java/com/springboot/monew/comment/repository/CommentRepository.java
+++ src/main/java/com/springboot/monew/comment/repository/CommentRepository.java
@@ -2,8 +2,12 @@ package com.springboot.monew.comment.repository;
 
 import com.springboot.monew.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
+    Optional<Comment> findByIdAndIsDeletedFalse(UUID id);
 }
--- src/main/java/com/springboot/monew/comment/service/CommentService.java
+++ src/main/java/com/springboot/monew/comment/service/CommentService.java
@@ -1,17 +1,24 @@
 package com.springboot.monew.comment.service;
 
 import com.springboot.monew.comment.dto.CommentDto;
+import com.springboot.monew.comment.dto.CommentLikeDto;
 import com.springboot.monew.comment.dto.CommentRegisterRequest;
 import com.springboot.monew.comment.dto.CommentUpdateRequest;
 import com.springboot.monew.comment.entity.Comment;
+import com.springboot.monew.comment.entity.CommentLike;
+import com.springboot.monew.comment.mapper.CommentLikeMapper;
 import com.springboot.monew.comment.mapper.CommentMapper;
+import com.springboot.monew.comment.repository.CommentLikeRepository;
 import com.springboot.monew.comment.repository.CommentRepository;
+import com.springboot.monew.exception.ErrorCode;
+import com.springboot.monew.exception.comment.CommentErrorCode;
 import com.springboot.monew.exception.comment.CommentException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Map;
 import java.util.UUID;
 
 @Slf4j
@@ -19,10 +26,12 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final CommentLikeRepository commentLikeRepository;
     // TODO : Article, User 구현 시 주석 해제
     // private final ArticleRepository articleRepository;
     // private final UserRepository userRepository;
     private final CommentMapper commentMapper;
+    private final CommentLikeMapper commentLikeMapper;
 
     // 댓글 등록
     @Transactional
@@ -46,16 +55,49 @@ public class CommentService {
     // 댓글 수정
     @Transactional
     public CommentDto update(UUID commentId, UUID userId, CommentUpdateRequest request){
-        // comment 조회 Todo: 일단 예외 보류
-        Comment comment = commentRepository.findById(commentId)
-                .orElseThrow();
+        // comment 조회 (존재, SoftDelete 여부 체크)
+        Comment comment = commentRepository.findByIdAndIsDeletedFalse(commentId)
+                .orElseThrow(() -> new CommentException(CommentErrorCode.COMMENT_NOT_FOUND, Map.of("commentId", commentId)));
 
-        // user 본인이 단 댓글인 지 확인
-        // commentRepository.existsCommentByUserId();
+        // user 조회 (존재, SoftDelete 여부 체크)
+
+        // TODO: user 본인이 단 댓글인 지 확인, User 구현 시 주석 해제
+        // if (!comment.getUser().getId().equals(userId)){
+        // throw new CommentException(CommentErrorCode.COMMENT_NOT_OWNED_BY_USER, Map.of("commentId", commentId));
+        // }
 
         // update 하고 Dto 변환
         comment.updateContent(request.content());
+        boolean likeByMe = false;
+                // TODO: 임시 값 false 해제 commentLikeRepository.existsByCommentIdAndUserId(commentId, userId);
 
-        return commentMapper.toCommentDto(comment, false);
+        log.info("댓글 수정 완료 - commentId: {} userId: {}", commentId, userId);
+        log.debug("댓글 수정 내용 - newContent: {}", comment.getContent());
+        return commentMapper.toCommentDto(comment,likeByMe);
+    }
+
+    @Transactional
+    public CommentLikeDto like(UUID commentId, UUID userId){
+        // 댓글 조회 (존재, SoftDelete 여부)
+        Comment comment = commentRepository.findByIdAndIsDeletedFalse(commentId)
+                .orElseThrow(() -> new CommentException(CommentErrorCode.COMMENT_NOT_FOUND, Map.of("commentId", commentId)));
+
+        // Todo: User 조회 (존재, SoftDelete 여부)
+        // User user = userRepository.findByIdAndDeleteAt~(userId)
+
+        // Todo: 중복 좋아요 check
+//        if(!commentLikeRepository.existsByIdAndUserId(commentId, userId)){
+//            throw new CommentException(CommentErrorCode.COMMENT_LIKE_ALREADY_EXISTS,
+//                    Map.of("commentId", commentId, "userId", userId));
+//        }
+
+        // CommentLike 생성 후 save && Comment의 likeCount ++
+        // Todo: User 구현 시 로직 수정
+        CommentLike commentLike = new CommentLike(comment);
+        commentLikeRepository.save(commentLike);
+        comment.increaseLikeCount();
+
+        // Comment, CommentLike -> CommentLikeDto로 변환
+        return commentLikeMapper.toCommentLikeDto(commentLike);
     }
 }
