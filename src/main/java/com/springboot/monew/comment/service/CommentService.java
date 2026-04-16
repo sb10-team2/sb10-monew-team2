428e4e04b9a5b435e58b29d1e8da0186037b8548
jsj <sungju3115@naver.com>
2026-04-16 10:57:49 +0900

    wip: 댓글 수정 구현 중

--- src/main/java/com/springboot/monew/comment/service/CommentService.java
+++ src/main/java/com/springboot/monew/comment/service/CommentService.java
@@ -2,12 +2,19 @@ package com.springboot.monew.comment.service;
 
 import com.springboot.monew.comment.dto.CommentDto;
 import com.springboot.monew.comment.dto.CommentRegisterRequest;
+import com.springboot.monew.comment.dto.CommentUpdateRequest;
 import com.springboot.monew.comment.entity.Comment;
 import com.springboot.monew.comment.mapper.CommentMapper;
 import com.springboot.monew.comment.repository.CommentRepository;
+import com.springboot.monew.exception.comment.CommentException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.UUID;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CommentService {
@@ -18,6 +25,7 @@ public class CommentService {
     private final CommentMapper commentMapper;
 
     // 댓글 등록
+    @Transactional
     public CommentDto create(CommentRegisterRequest request){
         // TODO: Article, User 구현 시 주석 해제
         // 존재하는 기사인지 check
@@ -31,6 +39,23 @@ public class CommentService {
         // TODO: Article, User 추가
         Comment comment = new Comment(request.content());
         commentRepository.save(comment);
+        log.info("댓글 등록 완료 - commentId: {}, articleId: {}, userId: {}", comment.getId(), request.articleId(), request.userId());
+        return commentMapper.toCommentDto(comment, false);
+    }
+
+    // 댓글 수정
+    @Transactional
+    public CommentDto update(UUID commentId, UUID userId, CommentUpdateRequest request){
+        // comment 조회 Todo: 일단 예외 보류
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow();
+
+        // user 본인이 단 댓글인 지 확인
+        // commentRepository.existsCommentByUserId();
+
+        // update 하고 Dto 변환
+        comment.updateContent(request.content());
+
         return commentMapper.toCommentDto(comment, false);
     }
 }
