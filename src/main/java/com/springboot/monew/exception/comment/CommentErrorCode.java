70cd9da563f183c7c214a8b1cd91e64b23b9e304
jsj <sungju3115@naver.com>
2026-04-16 15:36:19 +0900

    feat: 중복 삭제 에러 코드 추가

--- src/main/java/com/springboot/monew/exception/comment/CommentErrorCode.java
+++ src/main/java/com/springboot/monew/exception/comment/CommentErrorCode.java
@@ -11,7 +11,8 @@ public enum CommentErrorCode implements ErrorCode {
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "CM01", "댓글을 찾을 수 없습니다."),
     COMMENT_NOT_OWNED_BY_USER(HttpStatus.FORBIDDEN, "CM02", "본인의 댓글이 아닙니다."),
     COMMENT_LIKE_ALREADY_EXISTS(HttpStatus.CONFLICT, "CM03", "이미 좋아요를 눌렀습니다."),
-    COMMENT_LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "CM04", "좋아요를 누르지 않았습니다.");
+    COMMENT_LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "CM04", "좋아요를 누르지 않았습니다."),
+    COMMENT_ALREADY_DELETED(HttpStatus.BAD_REQUEST, "CM05", "이미 삭제한 댓글입니다." );
 
     private final HttpStatus httpStatus;
     private final String code;
