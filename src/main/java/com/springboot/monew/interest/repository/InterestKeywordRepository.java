66b6d480c1796c97558272eac84d6fc4e93dfa68
kwakinsung <kis2690@gmail.com>
2026-04-17 13:31:04 +0900

    feat: 관심사의 키워드 목록 메서드 추가

--- src/main/java/com/springboot/monew/interest/repository/InterestKeywordRepository.java
+++ src/main/java/com/springboot/monew/interest/repository/InterestKeywordRepository.java
@@ -3,7 +3,11 @@ package com.springboot.monew.interest.repository;
 import com.springboot.monew.interest.entity.InterestKeyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface InterestKeywordRepository extends JpaRepository<InterestKeyword, UUID> {
+
+    //관심사의 키워드 목록
+    List<String> findAllKeywords();
 }
