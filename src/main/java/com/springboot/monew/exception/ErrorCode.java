fec6c94e958cae726b3bf10d6adc8ecb72eeb288
kwakinsung <kis2690@gmail.com>
2026-04-17 17:30:35 +0900

    refactor: dev 반영위해 exception 패키지 삭제

--- src/main/java/com/springboot/monew/exception/ErrorCode.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.monew.exception;
-
-import org.springframework.http.HttpStatus;
-
-public interface ErrorCode {
-    HttpStatus getHttpStatus();
-    String getCode();
-    String getMessage();
-}
