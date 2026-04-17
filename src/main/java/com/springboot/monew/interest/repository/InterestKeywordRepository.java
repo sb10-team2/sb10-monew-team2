c0d393ea5aa6aa1239c88aeab04a65ee9db336e8
kwakinsung <kis2690@gmail.com>
2026-04-17 16:47:18 +0900

    refactor: 관심사 키워드 조회 메서드 수정

--- src/main/java/com/springboot/monew/interest/repository/InterestKeywordRepository.java
+++ src/main/java/com/springboot/monew/interest/repository/InterestKeywordRepository.java
@@ -2,6 +2,7 @@ package com.springboot.monew.interest.repository;
 
 import com.springboot.monew.interest.entity.InterestKeyword;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
@@ -9,5 +10,6 @@ import java.util.UUID;
 public interface InterestKeywordRepository extends JpaRepository<InterestKeyword, UUID> {
 
     //관심사의 키워드 목록
+    @Query("select ik.keyword.name from InterestKeyword ik")
     List<String> findAllKeywords();
 }
