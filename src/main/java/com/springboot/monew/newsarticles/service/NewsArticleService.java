a02ba1a5ee4441b1ff26847de68cd6403c19ca79
kwakinsung <kis2690@gmail.com>
2026-04-17 15:06:50 +0900

    refactor: saveAll stream으로 수정

--- src/main/java/com/springboot/monew/newsarticles/service/NewsArticleService.java
+++ src/main/java/com/springboot/monew/newsarticles/service/NewsArticleService.java
@@ -22,21 +22,28 @@ public class NewsArticleService {
     private final NewsArticleRepository newsArticleRepository;
 
     @Transactional
-    public void saveAll(List<CollectedArticle> collectedArticles) {
-        for (CollectedArticle item : collectedArticles) {
-            if (newsArticleRepository.existsByOriginalLink(item.originalLink())) {
-                continue;
-            }
+    public void saveAll(List<CollectedArticle> articles) {
 
-            NewsArticle article = new NewsArticle(
-                    item.source().name(),
-                    item.originalLink(),
-                    item.title(),
-                    item.publishedAt(),
-                    item.summary()
-            );
+        //기사 하나마다 existsByOriginalLink()로 DB조회 1번씩 하게됨.
+        //ex) 100개 기사 -> 100번 existsByOriginalLink 조회.
+        //개선 필요.
+        List<NewsArticle> entities = articles.stream()
+                .filter(article -> !newsArticleRepository.existsByOriginalLink(article.originalLink()))
+                .map(this::toEntity)
+                .toList();
 
-            newsArticleRepository.save(article);
-        }
+        newsArticleRepository.saveAll(entities);
+
+    }
+
+    //DB저장을 위해 NewsArticle Entity형으로 변환
+    private NewsArticle toEntity(CollectedArticle article) {
+        return NewsArticle.builder()
+                .source(article.source())
+                .originalLink(article.originalLink())
+                .title(article.title())
+                .publishedAt(article.publishedAt())
+                .summary(article.summary())
+                .build();
     }
 }
