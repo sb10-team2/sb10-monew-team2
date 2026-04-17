09ccf3a2d5076cd5777227a1db776acaf05ec2c0
kwakinsung <kis2690@gmail.com>
2026-04-17 13:32:48 +0900

    refactor: 스케줄러 로깅 추가

--- src/main/java/com/springboot/monew/newsarticles/scheduler/NewsArticleScheduler.java
+++ src/main/java/com/springboot/monew/newsarticles/scheduler/NewsArticleScheduler.java
@@ -2,17 +2,23 @@ package com.springboot.monew.newsarticles.scheduler;
 
 import com.springboot.monew.newsarticles.service.NewsArticleCollectService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class NewsArticleScheduler {
 
     private final NewsArticleCollectService newsArticleCollectService;
 
+    //매 시간 0분 0초 newsArticleCollectService.collectAll()을 실행한다.
     @Scheduled(cron = "0 0 * * * *")
     public void collectArticlesEveryHour() {
+
+        log.info("뉴스 기사 수집 배치 시작");
         newsArticleCollectService.collectAll();
+        log.info("뉴스 기사 수집 배치 종료");
     }
 }
