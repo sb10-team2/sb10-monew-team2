49d3ee4791c59ff24b510efbbe2fd2598393ee2e
JunCH163 <ckdgus163@gmail.com>
2026-04-20 21:22:39 +0900

    feat: 자동 물리 삭제 스케줄러 구현

--- src/main/java/com/springboot/monew/users/repository/UserRepository.java
+++ src/main/java/com/springboot/monew/users/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.springboot.monew.users.repository;
 
 import com.springboot.monew.users.entity.User;
+import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,4 +18,14 @@ public interface UserRepository extends JpaRepository<User, UUID> {
   boolean existsByNickname(String nickname);
 
   Optional<User> findByEmail(String email);
+
+  // 논리 삭제된 user들 중에서 현재 시간에서 24시간을 뺀 시간인, cutoff보다 더 과거에 deletedAt이 찍힌 user들을 조회
+  @Query("""
+          select  u
+          from User u
+          where u.deletedAt is not null
+            and u.deletedAt < :cutoff
+      """)
+  List<User> findUsersDeletedBefore(@Param("cutoff") Instant cutoff);
+
 }
