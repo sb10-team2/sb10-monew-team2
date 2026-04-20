b98f980abd952e55a6a6d15691f59f7af0eb09bb
jh9dev <sungjuhyun1001@gmail.com>
2026-04-20 11:44:34 +0900

    feat: INTEREST_NOT_FOUND 예외코드 추가

--- src/main/java/com/springboot/monew/interest/exception/InterestErrorCode.java
+++ src/main/java/com/springboot/monew/interest/exception/InterestErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum InterestErrorCode implements ErrorCode {
 
     INTEREST_NAME_DUPLICATION(HttpStatus.CONFLICT, "IN01", "유사한 이름의 관심사가 이미 존재합니다."),
-    DUPLICATE_KEYWORDS(HttpStatus.BAD_REQUEST, "IN02", "키워드는 중복될 수 없습니다.");
+    DUPLICATE_KEYWORDS(HttpStatus.BAD_REQUEST, "IN02", "키워드는 중복될 수 없습니다."),
+    INTEREST_NOT_FOUND(HttpStatus.NOT_FOUND, "IN03", "관심사를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
