dfed76a31df497f04c68197df66865e13a34e501
jsj <sungju3115@naver.com>
2026-04-20 15:14:57 +0900

    test: CommentController 테스트 추가

---
+++ src/test/java/com/springboot/monew/comment/controller/CommentControllerTest.java
@@ -0,0 +1,36 @@
+package com.springboot.monew.comment.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CommentControllerTest {
+
+  @Test
+  void list() {
+  }
+
+  @Test
+  void create() {
+  }
+
+  @Test
+  void like() {
+  }
+
+  @Test
+  void unlike() {
+  }
+
+  @Test
+  void softDelete() {
+  }
+
+  @Test
+  void update() {
+  }
+
+  @Test
+  void hardDelete() {
+  }
+}
