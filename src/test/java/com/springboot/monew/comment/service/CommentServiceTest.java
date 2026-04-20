9380c833b04a9c3231c3c9378893ce1edfe04bfe
jsj <sungju3115@naver.com>
2026-04-20 14:12:21 +0900

    test: CommentServiceTest 클래스 기본 구조 추가

---
+++ src/test/java/com/springboot/monew/comment/service/CommentServiceTest.java
@@ -0,0 +1,36 @@
+package com.springboot.monew.comment.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CommentServiceTest {
+
+  @Test
+  void create() {
+  }
+
+  @Test
+  void update() {
+  }
+
+  @Test
+  void like() {
+  }
+
+  @Test
+  void softDelete() {
+  }
+
+  @Test
+  void hardDelete() {
+  }
+
+  @Test
+  void unlike() {
+  }
+
+  @Test
+  void list() {
+  }
+}
