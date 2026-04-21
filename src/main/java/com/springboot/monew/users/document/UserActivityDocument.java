8a929c335c6ea43b994b4bba6266d7ce2e33f91b
JunCH163 <ckdgus163@gmail.com>
2026-04-22 00:01:59 +0900

    docs: UserActivityDocument 설명 주석 추가

--- src/main/java/com/springboot/monew/users/document/UserActivityDocument.java
+++ src/main/java/com/springboot/monew/users/document/UserActivityDocument.java
@@ -11,6 +11,19 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+/*
+  사용자 활동 내역 조회를 위한 MongoDB 저장 모델이다.
+
+  사용자별 활동 데이터를 한 문서에 모아 저장하여 활동 내역 조회 시 RDBMS의 여러 테이블을 조합하지 않고
+  MongoDB 문서 하나로 응답 DTO를 구성할 수 있도록 한다.
+
+  이 모델은 API 응답 DTO와 유사한 구조를 가지지만, 응답 DTO를 직접 저장 타입으로
+  사용하지 않고 Document 전용 Item record를 사용해 API 응답 모델과 MongoDB 저장 모델의 책임을 분리한다.
+
+  아래 Item record들은 UserActivityDocument 내부 리스트에 저장되는 한 건의 데이터이다.
+  API 응답 DTO와 필드 구조는 유사하지만 MongoDB 저장 전용 타입으로 사용한다.
+ */
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Document(collection = "user_activities")
