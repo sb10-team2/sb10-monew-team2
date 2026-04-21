599672e570edbcb98fb54a595e7d7a0db49c1e70
jsj <sungju3115@naver.com>
2026-04-21 15:23:36 +0900

    test: ControllerTest 테스트 코드 추가

---
+++ src/test/java/com/springboot/monew/comment/controller/CommentLikeControllerTest.java
@@ -0,0 +1,16 @@
+package com.springboot.monew.comment.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CommentLikeControllerTest {
+
+  @Test
+  void like() {
+  }
+
+  @Test
+  void unlike() {
+  }
+}
