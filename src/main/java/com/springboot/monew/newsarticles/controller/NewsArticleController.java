acff0676096295192d2ae85858ee5e1409ece346
kwakinsung <kis2690@gmail.com>
2026-04-22 16:34:31 +0900

    feat: 뉴스기사 뷰 등록 Controller 설계

--- src/main/java/com/springboot/monew/newsarticles/controller/NewsArticleController.java
+++ src/main/java/com/springboot/monew/newsarticles/controller/NewsArticleController.java
@@ -1,7 +1,9 @@
 package com.springboot.monew.newsarticles.controller;
 
+import com.springboot.monew.newsarticles.dto.response.NewsArticleViewDto;
 import com.springboot.monew.newsarticles.service.NewsArticleCollectService;
 import com.springboot.monew.newsarticles.service.NewsArticleService;
+import java.net.URI;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +29,17 @@ public class NewsArticleController {
   public ResponseEntity<String> collectNews() {
     newsArticleCollectService.collectAll();
     return ResponseEntity.ok("뉴스 수집 완료");
+  }
+
+  //기사 뷰 등록
+  @PostMapping("/{articleId}/article-views")
+  public ResponseEntity<NewsArticleViewDto> createView(@PathVariable("articleId") UUID articleId,
+                                                       @RequestHeader("Monew-Request-User-ID") UUID userId) {
+    log.debug("뉴스기사 뷰 등록 요청: articleId={} userId={}", articleId, userId);
+
+    NewsArticleViewDto newsArticleViewDto = newsArticleService.createView();
+    return ResponseEntity.created(URI.create("/api/articles/" + articleId)).body(newsArticleViewDto);
+
   }
 
   //뉴스기사 논리삭제
