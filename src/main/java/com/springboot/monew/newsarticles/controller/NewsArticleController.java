67bc8d8ffee79877a1f863a7851e7371ab4645fe
kwakinsung <kis2690@gmail.com>
2026-04-22 15:16:39 +0900

    refactor: URI 수정

--- src/main/java/com/springboot/monew/newsarticles/controller/NewsArticleController.java
+++ src/main/java/com/springboot/monew/newsarticles/controller/NewsArticleController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/article")
+@RequestMapping("/api/articles")
 public class NewsArticleController {
 
   private final NewsArticleCollectService newsArticleCollectService;
