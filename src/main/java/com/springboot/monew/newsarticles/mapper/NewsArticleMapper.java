2128bc204274343117f4c29eac741db7f88b5497
kwakinsung <kis2690@gmail.com>
2026-04-23 13:38:58 +0900

    feat:뉴스기사 DTO Mapper 구현

--- src/main/java/com/springboot/monew/newsarticles/mapper/NewsArticleMapper.java
+++ src/main/java/com/springboot/monew/newsarticles/mapper/NewsArticleMapper.java
@@ -1,13 +1,20 @@
 package com.springboot.monew.newsarticles.mapper;
 
 import com.springboot.monew.newsarticles.dto.response.CollectedArticle;
+import com.springboot.monew.newsarticles.dto.response.NewsArticleDto;
 import com.springboot.monew.newsarticles.entity.NewsArticle;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface NewsArticleMapper {
 
   NewsArticle toEntity(CollectedArticle article);
+
+  @Mapping(target = "commentCount", source = "commentCount")
+  @Mapping(target = "viewedByMe", source = "viewedByMe")
+  @Mapping(target = "sourceUrl", source = "article.originalLink")
+  NewsArticleDto toDto(NewsArticle article, Long commentCount, Boolean viewedByMe);
 
 
 }
