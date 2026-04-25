e52970a8f17ba6f0e8df56563f33efd90518420a
전창현 <ckdgus163@gmail.com>
2026-04-25 20:45:04 +0900

    refactor: 활동내역 뉴스기사 조회 응답 DTO 통합

--- src/main/java/com/springboot/monew/users/mapper/UserActivityMapper.java
+++ src/main/java/com/springboot/monew/users/mapper/UserActivityMapper.java
@@ -1,6 +1,7 @@
 package com.springboot.monew.users.mapper;
 
 import com.springboot.monew.interest.dto.response.SubscriptionDto;
+import com.springboot.monew.newsarticles.dto.response.NewsArticleViewDto;
 import com.springboot.monew.users.document.UserActivityDocument;
 import com.springboot.monew.users.document.UserActivityDocument.ArticleViewItem;
 import com.springboot.monew.users.document.UserActivityDocument.CommentItem;
@@ -8,7 +9,6 @@ import com.springboot.monew.users.document.UserActivityDocument.CommentLikeItem;
 import com.springboot.monew.users.document.UserActivityDocument.SubscriptionItem;
 import com.springboot.monew.users.dto.response.CommentActivityDto;
 import com.springboot.monew.users.dto.response.CommentLikeActivityDto;
-import com.springboot.monew.users.dto.response.TemporaryArticleViewDto;
 import com.springboot.monew.users.dto.response.UserActivityDto;
 import com.springboot.monew.users.entity.User;
 import org.mapstruct.Mapper;
@@ -31,5 +31,5 @@ public interface UserActivityMapper {
 
   CommentLikeActivityDto toDto(CommentLikeItem item);
 
-  TemporaryArticleViewDto toDto(ArticleViewItem item);
+  NewsArticleViewDto toDto(ArticleViewItem item);
 }
