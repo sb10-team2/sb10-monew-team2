77f8fe121a3cafedb1d2ce3284a63db2419b22c6
전창현 <ckdgus163@gmail.com>
2026-04-25 20:40:29 +0900

    feat: 댓글 작성/수정/삭제 활동내역 이벤트 발행

--- src/main/java/com/springboot/monew/comment/mapper/CommentMapper.java
+++ src/main/java/com/springboot/monew/comment/mapper/CommentMapper.java
@@ -2,6 +2,7 @@ package com.springboot.monew.comment.mapper;
 
 import com.springboot.monew.comment.dto.CommentDto;
 import com.springboot.monew.comment.entity.Comment;
+import com.springboot.monew.users.document.UserActivityDocument.CommentItem;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -13,4 +14,11 @@ public interface CommentMapper {
   @Mapping(source = "comment.user.nickname", target = "userNickname")
   @Mapping(source = "likeByMe", target = "likeByMe")
   CommentDto toCommentDto(Comment comment, boolean likeByMe);
+
+  // 사용자 활동 문서에 저장할 댓글 활동 Item으로 변환한다.
+  @Mapping(source = "article.id", target = "articleId")
+  @Mapping(source = "article.title", target = "articleTitle")
+  @Mapping(source = "user.id", target = "userId")
+  @Mapping(source = "user.nickname", target = "userNickname")
+  CommentItem toCommentItem(Comment comment);
 }
