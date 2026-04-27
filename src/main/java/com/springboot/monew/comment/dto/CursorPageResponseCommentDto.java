1fbbb3b51adf8c25b9f21648160d1ec8d861777e
jsj <sungju3115@naver.com>
2026-04-27 15:12:41 +0900

    fix: 못지운 주석 제거

--- src/main/java/com/springboot/monew/comment/dto/CursorPageResponseCommentDto.java
+++ src/main/java/com/springboot/monew/comment/dto/CursorPageResponseCommentDto.java
@@ -4,7 +4,6 @@ import java.time.Instant;
 import java.util.List;
 
 // 페이지네이션 응답 Dto
-// Todo: 디폴트 값 설정 추가
 public record CursorPageResponseCommentDto<T>(
     List<T> content,
     String nextCursor,
