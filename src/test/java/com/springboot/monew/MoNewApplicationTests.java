108c035a37cae164d29973577e2b72bb4a146d80
kwakinsung <kis2690@gmail.com>
2026-04-28 18:09:41 +0900

    refactor: application-test.yml 사용

--- src/test/java/com/springboot/monew/MoNewApplicationTests.java
+++ src/test/java/com/springboot/monew/MoNewApplicationTests.java
@@ -2,12 +2,14 @@ package com.springboot.monew;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(properties = {
     "spring.datasource.url=jdbc:h2:mem:testdb-boot;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=PostgreSQL"
 })
+@ActiveProfiles("test")
 class MoNewApplicationTests {
 
   @Test
